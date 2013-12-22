d74529ad1dd1a35f6c7b1bee7628b08b1da5ae81
Spencer <sepmein@gmail.com>
2013-12-22 23:06:37 +0800

    changed test setting, setup work env on ubuntu

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
-describe('Controller: pcoApp.controllers.main', function () {
-
+describe('Controller: pcoApp.controllers.main', function() {
+	describe('Homepage', function() {
+		it('should display the correct route', function() {
+			expect(1).toBe(1);
+		});
+	});
 });
