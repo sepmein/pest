814d15c7b4190fa410414548e4d33d613e095c3f
Spencer <sepmein@gmail.com>
2013-12-23 17:16:52 +0800

    added intellegence multi select/unselect function to pco

--- app/scripts/controllers/pco/list.js
+++ app/scripts/controllers/pco/list.js
@@ -1,18 +1,19 @@
 'use strict';
 
 angular.module('pcoApp')
-	.controller('pcoApp.controllers.list', ['$scope', '$firebase', 'FireRefs', 'orderByPriorityFilter', 'filterFilter', 'print',
-		function($scope, $firebase, FireRefs, orderByPriorityFilter, filterFilter, print) {
-			//bind to firebase database
+	.controller('pcoApp.controllers.list', ['$scope', '$firebase', 'FireRefs', '$filter', 'print',
+		function($scope, $firebase, FireRefs, $filter, print) {
+			/*init*/
 			$scope.pcoList = $firebase(FireRefs.pcoList());
-
-			var filteredArray = filterFilter(orderByPriorityFilter($scope.pcoList), $scope.q);
-
-			//tri-state : none／part／all
-			// select = 1 && unselected = 1   =>    part
-			// select = 1 && unselected = 0   =>    all
-			// select = 0 && unselected = 1   =>    none
 			$scope.printList = print.list;
+			$scope.language = 'cn';
+
+
+			//refresh FilteredArray when search query changes
+			//use $filter service to call filters in the controller
+			$scope.$watch('$scope.q', function(newVal, oldVal) {
+				$scope.filteredArray = $filter('filter')($filter('orderByPriority')($scope.pcoList), newVal);
+			});
 
 			$scope.printCount = function() {
 				var count = 0;
@@ -23,34 +24,30 @@ angular.module('pcoApp')
 				}
 				return count;
 			};
-			/*			$scope.changeTriStateSelectBoxState = function() {
-				$scope.printList.state.selected = 0;
-				$scope.printList.state.unselected = 0;
-				for (var pco in $scope.pcoList) {
-					//只要列表中有选中的，那有选中的状态为true
-					$scope.printList.state.selected = $scope.printList.state.selected || $scope.printList[pco];
-					//只要列表中有未选中的，那未选中的状态为true
-					$scope.printList.state.unselected = $scope.printList.state.unselected || !$scope.printList[pco];
-				}
-				//只要有为选中的，那总选择为false
-				$scope.printList.state.unselected ? $scope.triStateSelectBox = false : $scope.triStateSelectBox = true;
+
+			/**
+			 * print list structure
+			 * {
+			 *	id1 : true,
+			 *	id3 : false,
+			 *	...
+			 * }
+			 * ids comes from firebase pco.list.ids -> orderByPriority -> filteredArray
+			 */
+			$scope.selectAll = function() {
+				for (var i = $scope.filteredArray.length - 1; i >= 0; i--) {
+					$scope.printList[$scope.filteredArray[i].$id] = true;
+				};
+				console.log($scope.filteredArray);
 			};
 
-			$scope.triStateSelect = function() {
-				//若有未选中的，则全选，否则全部取消
-				if ($scope.printList.state.selected && $scope.printList.state.unselected) {
-					for (var key in $scope.pcoList) {
-						$scope.printList[key] = true;
-					}
-				} else {
-					for (var key in $scope.pcoList) {
-						$scope.printList[key] = false;
-					}
-				}
-			};*/
+			$scope.deSelectAll = function() {
+				for (var i = $scope.filteredArray.length - 1; i >= 0; i--) {
+					$scope.printList[$scope.filteredArray[i].$id] = false;
+				};
+			};
 
 			//中英文切换
-			$scope.language = 'cn';
 			$scope.translation = function() {
 				var l = $scope.language;
 				$scope.language = ((l === 'cn') ? 'en' : 'cn');
